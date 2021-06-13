2d9dafaff9177ede65b72daf97dd7d5b9134f2a5
BHedi89 <hedi.besenyei89@gmail.com>
2021-06-13 14:36:54 +0200

    toggle expense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [showExpenseForm, setExpenseForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpanseData) => {
     const expenseData = {
       ...enteredExpanseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setExpenseForm(false);
   };
+
+  const toggleExpenseHandler = () => {
+    setExpenseForm(true);
+  }
+
+  const cancelEditing = () => {
+    setExpenseForm(false);
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
+      {!showExpenseForm && <button onClick={toggleExpenseHandler}>Add New Expense</button>}
+      {showExpenseForm && 
+        <ExpenseForm 
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelEditing}>
+        </ExpenseForm>}
     </div>
   );
 };
